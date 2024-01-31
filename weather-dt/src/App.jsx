d1ef238105dcbd4db56233b17dabbcd47cd1e8e4
Aryan Bhosale <aryan.bhosale1719@gmail.com>
2024-01-31 16:40:31 +0530

    dynamic render

--- weather-dt/src/App.jsx
+++ weather-dt/src/App.jsx
@@ -1,17 +1,26 @@
 import Header from "./components/Header";
 import Render from "./components/Render";
 import Parameters from "./components/Parameters";
+import { useState } from "react";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState("Snow");
+  const [forecastWeather, setForecastWeather] = useState("Rain");
+  const [selectedWeather, setSelectedWeather] = useState(currentWeather);
   return (
     // main div container
     <div className="flex flex-col h-full w-full justify-center items-center bg-[#030712]">
       {/* Header Component Container */}
-        <Header />
+      <Header
+        currentWeather={currentWeather}
+        forecastWeather={forecastWeather}
+        selectedWeather={selectedWeather}
+        setSelectedWeather={setSelectedWeather}
+      />
       {/* Main page below header container */}
       <div className="flex flex-row w-full justify-center items-center divide-x-2 divide-gray-800">
         {/* 3D renderer component */}
-        <Render />
+        <Render selectedWeather={selectedWeather} />
         {/* Parameter display component */}
         <Parameters />
       </div>
--- weather-dt/src/components/Header.jsx
+++ weather-dt/src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({ currentWeather, forecastWeather, selectedWeather, setSelectedWeather }) => {
   return (
     <div className='flex divide-x-2 divide-gray-800 h-32 flex-row w-full items-center justify-center text-center sticky inset-x-0 top-0 z-30 border-b border-gray-700 bg-black/75 backdrop-blur-sm transition-all rounded-2xl'>
-        <div className='flex justify-center items-center text-center w-full h-full cursor-pointer'>
+        <div className='flex justify-center items-center text-center w-full h-full cursor-pointer' onClick={() => setSelectedWeather(currentWeather)}>
             Current Conditions
         </div>
-        <div className='flex justify-center items-center text-center w-full h-full cursor-pointer'>
+        <div className='flex justify-center items-center text-center w-full h-full cursor-pointer' onClick={() => setSelectedWeather(forecastWeather)}>
             Forecast
         </div>
     </div>
