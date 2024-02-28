57dd733b44b66c8c4b69866aa7e1412e9be36736
raiiasingh19 <f20210278@goa.bits-pilani.ac.in>
2024-02-28 16:11:59 +0530

    raiias-push

--- weather-dt/src/components/Header.jsx
+++ weather-dt/src/components/Header.jsx
@@ -1,4 +1,9 @@
 import snowicon from "../assets/snowy.png";
+import rainyicon from "../assets/rainy.png";
+import calendaricon from "../assets/calendar-regular.svg";
+import clockicon from "../assets/clock-regular.svg";
+import orbicon from "../assets/crystal-ball.png";
+import { format } from "date-fns";
 
 const Header = ({ currentWeather, forecastWeather, setSelectedWeather }) => {
   return (
@@ -6,35 +11,43 @@ const Header = ({ currentWeather, forecastWeather, setSelectedWeather }) => {
       <div className="flex justify-center items-center text-center w-full h-full cursor-pointer hover:bg-gray-800 active:bg-slate-950 transition-all" onClick={() => setSelectedWeather(currentWeather)}>
         <div className="flex flex-row justify-around h-full w-full pt-5">
            
-          <div className="flex flex-col justify-start">
-          <div><h1 className="text-lg border border-gray-700"> Now</h1></div>
-          <div>
-            <p >2 degrees </p>
-            <p>Feels like 1.5 degrees</p>
+          
+          <div><div className="text-lg border border-gray-700 flex flex-row justify-center">Current Conditions <p className="p-2"><img src={clockicon} className="h-4 w-4"/></p></div>
+          <div className="flex flex-row justify-center space-x-20 mt-5">
+            <p> 2 degrees </p>
+            <div className="flex flex-col justify-start">
+            <p className="flex flex-row space-x-2 justify-center"><p>Heavy snow</p> <p><img src={snowicon} className="h-5 w-5"/></p></p>
+            </div>
+          </div>
+          
+          </div>
+          <div className="flex justify-end">
+          <div className="flex flex-col  border-gray-700">
+            <p className="flex justify-center space-y-10"><img src={calendaricon} className="h-5 w-5"/></p>
+            <p>{format(new Date(), "hh:mmaaaaa'm' dd MMM, yyyy")}</p>
+            
           </div>
           </div>
-          <div className="flex flex-col justify-start">
-           <p className="flex flex-row space-x-2 justify-center"><p>Heavy snow</p> <p><img src={snowicon} className="h-5 w-5"/></p></p>
-          </div>
+          
         </div>
       </div>
       <div className="flex justify-center items-center text-center w-full h-full cursor-pointer hover:bg-gray-800 active:bg-slate-950 transition-all" onClick={() => setSelectedWeather(forecastWeather)}>
-        <div className="flex flex-row justify-center items-center text-center">
-          <div className="flex flex-col justify-center items-center text-center">
-            <p>Forecast</p>
-            <p>15 degrees *icons*</p>
-            <p>Feels like 12 degrees</p>
-          </div>
-          <div className="flex flex-col justify-center items-center text-center">
-            <p>Rain</p>
-            <p>Precipitation: 70%</p>
-            <p>Wind: 20km/h, medium</p>
-            <p>Humidity: 85%</p>
-          </div>
-        </div>
+      <div className="flex flex-row justify-around h-full w-full pt-5">
+           
+          
+           <div><div className="text-lg border border-gray-700 flex flex-row justify-center">Forecast<p className="p-2"><img src={orbicon} className="h-4 w-4"/></p></div>
+           <div className="flex flex-row justify-center space-x-20 mt-5">
+             <p> 15 degrees </p>
+             <div className="flex flex-col justify-start">
+             <p className="flex flex-row space-x-2 justify-center"><p>Rain</p> <p><img src={rainyicon} className="h-5 w-5"/></p></p>
+             </div>
+           </div>
+           </div>
+           
+         </div>
       </div>
     </div>
   );
 };
 
-export default Header
+export default Header;
